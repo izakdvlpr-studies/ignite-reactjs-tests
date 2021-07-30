a744f09ca55c815d9274eddf52f5e37086c3fc53
zevdvlpr <zevdvlpr@gmail.com>
2021-07-30 18:41:16 -0300

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import { AppProps } from 'next/app';
+import { Provider as NextAuthProvider } from 'next-auth/client';
+
+import { Header } from '../components/Header';
+
+import '../styles/global.scss';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <NextAuthProvider session={pageProps.session}>
+      <Header />
+
+      <Component {...pageProps} />
+    </NextAuthProvider>
+  );
+}
